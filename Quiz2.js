e6b45d0d29559e4d96c27dc263b3e2017c6c3d1c
briansutachan <bsutachan@techlaunch.io>
2018-05-20 20:42:08 -0400

    Done

---
+++ Quiz2.js
@@ -0,0 +1,74 @@
+/*  1. Name and describe the two main operations of a stack (to add and remove data). */
+push(); /* It Adds to the beginning of an object.*/
+pop(); /* It Removes from the end of an object.*/
+
+/* 2. Name and describe the two main operations of a queue (to add and remove data). */
+enqueue(); /* It Adds a value to the beginning of a queue*/
+dequeue(); /* It Removesthe first value of a queue*/
+
+/* 3. Draw the tree resulting from adding the following sequence of numbers to an empty tree: 30, 45, 15, 10, 50, 40, 20, 27 */
+// //----------30---------
+//          /       \
+// //-----15--------45----
+//      /   \      /   \
+// //--10---20----40----50
+//            \
+// //---------27---------- 
+
+/* 4. Is this tree balanced? If not, which rotation needs to be done? (Use the following rotation as an example: rightRotation(30), or leftRotation(10)) */
+/* The tree is balanced. */
+
+/* 5 . Now add 29. Is the tree balanced? If not, which rotation needs to be done? (Use the following rotation as an example: rightRotation(30), or leftRotation(10)) */
+    //          30
+    //        /    \
+    //      15      45
+    //    /    \   /  \
+    //  10     20 40   50
+    //           \
+    //           27
+    //             \
+    //              29
+    /* It needs rotation. (10) */
+
+    /* 6. Consider the following tree:
+                   ------5
+                  ---2-----8
+                 -1--3 
+                 
+    /* Now add 0 to the tree. Which one is the first node to go out of balance? */ /* ==> 5 */
+
+/* 7. How do you fix this node? (Use the following rotation as an example: rightRotation(30), or leftRotation(10)) */
+
+
+/* 8. What are the four main steps of mergesort? */
+/* 
+- Identify the Base Case.
+- Divide.
+- Conquer.
+- Combine.
+*/
+
+/* 9. ay you have a program which handles the login queue to a game server. The game server is able to log in one person every one second. Assume that one second must elapse after a person logs in with an empty queue before they are removed from the queue. Draw the state of the queue at 12:00:06, considering the following sequence of events:
+
+a- At 12:00:00 Hades logs in
+b- At 12:00:00 Ares logs in
+c- At 12:00:00 Zeus logs in
+d- At 12:00:00 Buzz Light Year logs in
+e- At 12:00:02 Kanye West logs in
+f- At 12:00:03 Taylor Swift logs in
+g- At 12:00:03 Darkwing Duck logs in
+h- At 12:00:04 Evil Mickey logs in. */
+
+/* 10. Solve https://www.hackerrank.com/challenges/compare-the-triplets - be mindful that you are required to print the output to the console in exactly the format that was asked. You are not required to return a value, just print to the console. Also be mindful to use JavaScript. */
+
+
+
+
+
+
+/* 11. Solve https://www.hackerrank.com/challenges/mini-max-sum - be mindful that you are required to print the output to the console in exactly the format that was asked. You are not required to return a value, just print to the console. Also be mindful to use JavaScript. */
+
+
+
+
+
